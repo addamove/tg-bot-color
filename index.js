d4520b1b0887cb64f07efba328289109d9f2d7cb
likelikerepost <empire.empo@gmail.com>
2017-12-12 19:11:34 +0300

    done

--- index.js
+++ index.js
@@ -1,239 +1,154 @@
 var TelegramBot = require("node-telegram-bot-api");
-
 var token = "390609420:AAGgOQfAmlP8sP9HrrVmpitIwyocvCUgQwU";
+var bot = new TelegramBot(token, { polling: true });
 
-const bot = new TelegramBot(token, { polling: true });
+let state = {
+  color: 0,
+  i: 0,
+  again: false
+};
 
-anwser = color => {
+let askQuestion = questions => {
+  questions.map(question => {});
+};
+
+var questions = [
+  {
+    title: "Кошки, цветы, аппельсины, конфеты?",
+    buttons: [
+      [
+        {
+          text: "кошки",
+          callback_data: "1_1"
+        }
+      ],
+      [
+        {
+          text: "цветы",
+          callback_data: "1_2"
+        }
+      ],
+      [
+        {
+          text: "аппельсины",
+          callback_data: "1_3"
+        }
+      ],
+      [{ text: "конфеты", callback_data: "1_4" }]
+    ]
+  },
+  {
+    title: "Стакан, банан, ананас, мандарин?",
+    buttons: [
+      [{ text: "Стакан", callback_data: "2_1" }],
+      [{ text: "банан", callback_data: "2_2" }],
+      [{ text: "ананас", callback_data: "2_3" }],
+      [{ text: "мандарин", callback_data: "2_4" }]
+    ]
+  },
+  {
+    title: "Солнце или луна?",
+    buttons: [
+      [{ text: "солнце", callback_data: "3_1" }],
+      [{ text: "луна", callback_data: "3_2" }]
+    ]
+  }
+];
+
+function getQuestion() {
+  return questions[Math.floor(Math.random() * questions.length)];
+}
+
+function newQuestion(msg) {
+  var arr = questions[state.i];
+  var text = arr.title;
+  var options = {
+    reply_markup: JSON.stringify({
+      inline_keyboard: arr.buttons
+    })
+  };
+  chat = msg.hasOwnProperty("chat") ? msg.chat.id : msg.from.id;
+  bot.sendMessage(chat, text, options);
+  state.i++;
+}
+
+function answered(msg) {
   let swt = 0;
   let url;
-  if (color < 0) {
-    swt = -100;
-    url = "https://goo.gl/naz2jk";
-    //тёмно синий
-  }
-  switch (color) {
-    case 1:
-      swt = 100;
-      url = "https://goo.gl/j3ZEi6";
-      //голубой
-      break;
-    case 2:
-      swt = 200;
-      url = "https://goo.gl/6BQfPy";
-      //аквамарин
-      break;
+  let colour;
+
+  switch (state.color) {
     case 3:
-      swt = 300;
-      url = "https://goo.gl/pnDtsV";
-      //тёмно бирюзовый
+      url = "https://goo.gl/j3ZEi6";
+      colour = "голубой";
       break;
     case 4:
-      swt = 400;
+      url = "https://goo.gl/6BQfPy";
+      colour = "аквамарин";
+      break;
+    case 5:
+      url = "https://goo.gl/pnDtsV";
+      colour = "тёмно бирюзовый";
+      break;
+    case 6:
       url = "https://goo.gl/mxXC5o";
-      //электрик
+      colour = "электрик";
+      break;
+    case 7:
+      url =
+        "http://img1.liveinternet.ru/images/attach/c/9/108/191/108191007_RRRRRSSSRRS1.jpg";
+      colour = "кобальтовый";
+      break;
+    case 8:
+      url =
+        "https://otvet.imgsmail.ru/download/81140be2534899197ba5356842c43846_i-22.jpg";
+      colour = "ультрамарин";
+      break;
+    case 9:
+      url =
+        "http://www.alltiles.ru/Ispanskaya_plitka/Vives/images/Monocolor_Mar_20x20.jpg";
+      colour = "джинсовый";
       break;
 
     default:
   }
-  if (color >= 5) {
+  if (state.color >= 10) {
     swt = 500;
     url =
-      "https://demotivators.to/media/posters/1514/31831085_chto-to-poshlo-ne-tak.jpg";
-    //тёмно синий
+      "http://vyantex.ru/d/773069/d/%D0%A2%D0%B5%D0%BC%D0%BD.%D0%A1%D0%B8%D0%BD%D0%B8%D0%B9.jpg";
+    colour = "тёмно синий";
   }
-  return url;
-};
+  bot.sendPhoto(msg.from.id, url);
+  bot.sendMessage(msg.from.id, "Ваш оттенок синего определённо " + colour);
+  state.color = 0;
+  state.i = 0;
+  state.again = true;
+}
 
-bot.onText(/^\/selectseries/, function(msg, match) {
-  var seriesKB = [["Yes"], ["No"]]; // The keyboard array
-  bot.sendMessage(msg.chat.id, "Select a serie", {
-    reply_markup: {
-      keyboard: seriesKB,
-      one_time_keyboard: true
-    }
-  });
-  bot.onText(/.+/g, function(msg, match) {
-    bot.sendMessage(msg.chat.id, "You selected " + match);
-    var selectedSerie = msg.query;
-  });
+bot.onText(/\/fav_color/, function(msg, match) {
+  newQuestion(msg);
+  state.again = false;
 });
-
-bot.onText(/\/start/, (msg, match) => {
+bot.onText(/\/start/, function(msg, match) {
   bot.sendMessage(
-    msg.chat.id,
-    "- привет, хочешь узнать, какой цвет - твой? Если готов, то напиши ок"
+    msg.from.id,
+    "Я могу прочитать по звездам оттенок синего, который предназначен именно тебе! Для этого тебе придётся написать /fav_color мне в чат и выбрать три предмета, которые тебе нравятся больше всего из предложенных"
   );
-  bot.onText(/\ок/i, (msg, match) => {
-    started(msg, match);
-  });
 });
-let flowers = (msg, match, color) => {
-  bot.sendMessage(msg.chat.id, "Любите цветы? [люблю / не люблю]");
 
-  bot.onText(/\люблю/i, function(msg, match) {
-    color--;
+// Ответ от кнопок
+bot.on("callback_query", function(msg) {
+  if (!state.again) {
+    let answer = msg.data.split("_");
 
-    bot.sendMessage(msg.chat.id, "Розы или лилии?");
-    bot.onText(/\Розы/i, function(msg, match) {
-      color--;
-      bot.sendMessage(msg.chat.id, "Вам больше нравится шоколад или кекс?");
+    let button = answer[1];
 
-      bot.onText(/\шоколад/, function(msg, match) {
-        color++;
-        bot.sendMessage(msg.chat.id, "Дайте подумать");
-        bot.sendMessage(msg.chat.id, "Определяем ваш цвет");
-        setTimeout(function() {
-          //bot.sendMessage(msg.chat.id, anwser(color));
-          bot.sendMessage(msg.chat.id, "Определяем ваш цвет:" + color);
-          bot.sendPhoto(msg.chat.id, anwser(color));
-        }, 10000);
-      });
+    state.color = state.color + Number(button);
 
-      bot.onText(/\кекс/i, function(msg, match) {
-        color--;
-        bot.sendMessage(msg.chat.id, "Дайте подумать");
-        bot.sendMessage(msg.chat.id, "Определяем ваш цвет");
-        setTimeout(function() {
-          //bot.sendMessage(msg.chat.id, anwser(color));
-          bot.sendMessage(msg.chat.id, "Определяем ваш цвет:" + color);
-          bot.sendPhoto(msg.chat.id, anwser(color));
-        }, 10000);
-      });
-    });
-    bot.onText(/\Лилии/i, function(msg, match) {
-      color++;
-      bot.sendMessage(msg.chat.id, "Вам больше нравится шоколад или кекс?");
-
-      bot.onText(/\шоколад/i, function(msg, match) {
-        color--;
-        bot.sendMessage(msg.chat.id, "Дайте подумать");
-        bot.sendMessage(msg.chat.id, "Определяем ваш цвет");
-        setTimeout(function() {
-          //bot.sendMessage(msg.chat.id, anwser(color));
-          bot.sendMessage(msg.chat.id, "Определяем ваш цвет:" + color);
-          bot.sendPhoto(msg.chat.id, anwser(color));
-        }, 5000);
-      });
-
-      bot.onText(/\кекс/i, function(msg, match) {
-        color++;
-        bot.sendMessage(msg.chat.id, "Дайте подумать");
-        bot.sendMessage(msg.chat.id, "Определяем ваш цвет");
-        setTimeout(function() {
-          //bot.sendMessage(msg.chat.id, anwser(color));
-          bot.sendMessage(msg.chat.id, "Определяем ваш цвет:" + color);
-          bot.sendPhoto(msg.chat.id, anwser(color));
-        }, 10000);
-      });
-    });
-  });
-
-  bot.onText(/\не люблю/i, function(msg, match) {
-    color--;
-    bot.sendMessage(msg.chat.id, "Вам больше нравится кофе или чай?");
-
-    bot.onText(/\кофе/i, function(msg, match) {
-      color--;
-      bot.sendMessage(msg.chat.id, "Дайте подумать");
-      bot.sendMessage(msg.chat.id, "Определяем ваш цвет");
-      setTimeout(function() {
-        //bot.sendMessage(msg.chat.id, anwser(color));
-        bot.sendMessage(msg.chat.id, "Определяем ваш цвет:" + color);
-        bot.sendPhoto(msg.chat.id, anwser(color));
-      }, 10000);
-    });
-
-    bot.onText(/\чай/i, function(msg, match) {
-      color++;
-      bot.sendMessage(msg.chat.id, "Дайте подумать");
-      bot.sendMessage(msg.chat.id, "Определяем ваш цвет");
-      setTimeout(function() {
-        //bot.sendMessage(msg.chat.id, anwser(color));
-        bot.sendMessage(msg.chat.id, "Определяем ваш цвет:" + color);
-        bot.sendPhoto(msg.chat.id, anwser(color));
-      }, 10000);
-    });
-  });
-  return;
-};
-
-let animals = (msg, match, color) => {
-  bot.sendMessage(msg.chat.id, "Вам больше нравятся кошки или собаки?");
-
-  bot.onText(/\кошки/i, function(msg, match) {
-    color++;
-    bot.sendMessage(msg.chat.id, "Дикие или Домашние?");
-
-    bot.onText(/\Дикие/i, function(msg, match) {
-      color++;
-      bot.sendMessage(msg.chat.id, "Дайте подумать");
-
-      setTimeout(function() {
-        //bot.sendMessage(msg.chat.id, anwser(color));
-        bot.sendMessage(msg.chat.id, "Определяем ваш цвет:" + color);
-        bot.sendPhoto(msg.chat.id, anwser(color));
-      }, 10000);
-      return bot.sendMessage(msg.chat.id, "Закончили");
-    });
-
-    bot.onText(/\Домашние/i, function(msg, match) {
-      color++;
-      bot.sendMessage(msg.chat.id, "Дайте подумать");
-      bot.sendMessage(msg.chat.id, "Определяем ваш цвет");
-      setTimeout(function() {
-        //bot.sendMessage(msg.chat.id, anwser(color));
-        bot.sendMessage(msg.chat.id, "Определяем ваш цвет:" + color);
-        bot.sendPhoto(msg.chat.id, anwser(color));
-      }, 10000);
-    });
-  });
-
-  bot.onText(/\собаки/i, function(msg, match) {
-    color--;
-    bot.sendMessage(msg.chat.id, "Большие или Маленькие?");
-
-    bot.onText(/\Большие/i, function(msg, match) {
-      color++;
-      bot.sendMessage(msg.chat.id, "Дайте подумать");
-      bot.sendMessage(msg.chat.id, "Определяем ваш цвет");
-      setTimeout(function() {
-        //bot.sendMessage(msg.chat.id, anwser(color));
-        bot.sendMessage(msg.chat.id, "Определяем ваш цвет:" + color);
-        bot.sendPhoto(msg.chat.id, anwser(color));
-      }, 10000);
-    });
-    bot.onText(/\Маленькие/i, function(msg, match) {
-      color--;
-      bot.sendMessage(msg.chat.id, "Дайте подумать");
-      bot.sendMessage(msg.chat.id, "Определяем ваш цвет");
-      setTimeout(function() {
-        //bot.sendMessage(msg.chat.id, anwser(color));
-        bot.sendMessage(msg.chat.id, "Определяем ваш цвет:" + color);
-        bot.sendPhoto(msg.chat.id, anwser(color));
-      }, 10000);
-    });
-  });
-
-  return;
-};
-
-let started = (msg, match) => {
-  let color = 0;
-
-  // const chatId = msg.chat.id;
-
-  bot.sendMessage(msg.chat.id, "Ну тогда начинаем.");
-  bot.sendMessage(msg.chat.id, "Вам нравятся животные?");
-
-  bot.onText(/\да/i, function(msg, match) {
-    color++;
-    animals(msg, match, color);
-  });
-
-  bot.onText(/\нет/i, function(msg, match) {
-    color--;
-    flowers(msg, match, color);
-  });
-  return;
-};
+    if (state.i < questions.length) {
+      newQuestion(msg);
+    } else {
+      answered(msg);
+    }
+  }
+});
